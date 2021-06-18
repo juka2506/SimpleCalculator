a1b7d390a2939eda667e3d10d5eefd8bb64af37d
Julija Katukova <julija.katukova@gmail.com>
2021-06-18 21:23:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Number Declarations
+        int firstNumber;
+        int secondNumber;
+        int answer;
+
+        //Number Initializing
+        firstNumber = 5;
+        secondNumber = 7;
+        answer = 0;
+
+        //Mathematical operator
+        String operator = "-";
+
+        if(operator.equals("+")) {
+            answer = firstNumber + secondNumber;
+        } else if (operator.equals("-")) {
+            answer = firstNumber - secondNumber;
+        }
+
+        System.out.println(answer);
+    }
+}
